5832545549dd5a0ad39a8c02d5b9d6e8efa3e7f3
amorales <amorales@LAP-DESA06>
2024-05-21 13:10:16 -0500

    Agregar archivos de proyecto.

---
+++ PrintDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrintDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ PrintDemo/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿
+namespace PrintDemo;
+using PrintDemo.Helpers;
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+
+        (sender as Button).IsEnabled = false;
+
+        var fileName = "sample.pdf";
+        var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
+        var helper = new PrintHelper();
+
+#if WINDOWS
+        helper.Print(stream, fileName, this.Window.Handler.PlatformView as Microsoft.UI.Xaml.Window);
+
+#elif ANDROID
+        await helper.PrintAsync(stream, fileName);
+
+#elif __IOS__ || MACCATALYST
+		helper.Print(stream, fileName);
+
+#endif
+
+        (sender as Button).IsEnabled = true;
+    }
+
+
+}
+
+
